bd1350b6a1629bc5b85b6e0b28c2b93ec128ac54
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-27 13:04:39 +0430

    data shape added

--- machine-learning-2.py
+++ machine-learning-2.py
@@ -3,3 +3,9 @@ path = r"breast-cancer.data"
 headernames = ['Class','age','menopause','tumor-size','inv-nodes','node-caps','deg-malig','breast','breast-quad','irradiat']
 data = read_csv(path, names=headernames)
 print(data.head(50))
+
+#size
+print("************************* Shape *************************")
+print(data.shape)
+print("*********************************************************")
+
