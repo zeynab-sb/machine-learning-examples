8ead569d61066db908e107acaa2b94e0467a2981
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-27 13:55:59 +0430

    Load data added for machine-learning-3

---
+++ machine-learning-3.py
@@ -0,0 +1,9 @@
+from pandas import read_csv
+
+
+
+path = r"breast-cancer.data"
+headernames = ['Class','age','menopause','tumor-size','inv-nodes','node-caps','deg-malig','breast','breast-quad','irradiat']
+data = read_csv(path, names=headernames)
+print(data.head(50))
+
